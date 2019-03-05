3e249d6f104b70f01dd24dc536b7a9c10884a675
Jack Neto <jack@theworkinggroup.ca>
2019-03-04 20:31:50 -0500

    Small fixes (#147)

--- src/common/AutoComplete/__tests__/index.test.jsx
+++ src/common/AutoComplete/__tests__/index.test.jsx
@@ -9,13 +9,53 @@ describe('AutoComplete', () => {
     jest.clearAllMocks()
   })
 
-  it('matches snapshot', () => {
+  it('matches snapshot for simple select', () => {
     const component = renderer.create((
       <AutoComplete
         label="Institution"
         name="institution"
         options={[{ label: 'TD', value: 'TD' }]}
         onChange={mochOnChange}
+      />
+    ))
+    expect(component.toJSON()).toMatchSnapshot()
+  })
+
+  it('matches snapshot for async select', () => {
+    const component = renderer.create((
+      <AutoComplete
+        async
+        label="Institution"
+        name="institution"
+        options={[{ label: 'TD', value: 'TD' }]}
+        onChange={mochOnChange}
+      />
+    ))
+    expect(component.toJSON()).toMatchSnapshot()
+  })
+
+  it('matches snapshot for creatable select', () => {
+    const component = renderer.create((
+      <AutoComplete
+        creatable
+        label="Institution"
+        name="institution"
+        options={[{ label: 'TD', value: 'TD' }]}
+        onChange={mochOnChange}
+      />
+    ))
+    expect(component.toJSON()).toMatchSnapshot()
+  })
+
+  it('matches snapshot with error', () => {
+    const component = renderer.create((
+      <AutoComplete
+        label="Institution"
+        name="institution"
+        options={[{ label: 'TD', value: 'TD' }]}
+        onChange={mochOnChange}
+        error
+        helperText="Some error"
       />
     ))
     expect(component.toJSON()).toMatchSnapshot()
